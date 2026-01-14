7d87f27dc8deff28a18f805b8ae04272df6ebab3
Gwendal-B <12309@holbertonstudents.com>
2026-01-14 12:16:57 +0100

    fix: correction task 4

--- python-import_modules/4-hidden_discovery.py
+++ python-import_modules/4-hidden_discovery.py
@@ -1,17 +1,10 @@
 #!/usr/bin/python3
-import dis
-import marshal
-import types
-import sys
-import os
 
 if __name__ == "__main__":
-    pyc_file = "/tmp/hidden_4.pyc"
+    hidden = __import__("hidden_4")
 
-    with open(pyc_file, "rb") as f:
-        f.read(16)
-        code = marshal.load(f)
+    names = dir(hidden)
 
-    names = [name for name in code.co_names if not name.startswith("__")]
-    for name in sorted(names):
-        print(name)
+    for name in names:
+        if not name.startswith("__"):
+            print(name)
