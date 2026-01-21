2d4499440baf60b121f98d268583a4440d7eb4fe
Gwendal-B <12309@holbertonstudents.com>
2026-01-21 09:33:07 +0100

    fix: docstring correction in 0-add_integer.py

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,15 +1,10 @@
 #!/usr/bin/python3
-"""
-Module that defines a function to add two integers.
-Raises TypeError if inputs are not integers or floats.
-"""
+"""Add two integers."""
+
 def add_integer(a, b=98):
-    """Adds two integers and returns the result as an integer.
-        a (int, float): The first integer.
-        b (int, float): The second integer (default is 98).
-    """
-    if type(a) not in [int, float]:
+    """Return the addition of two integers."""
+    if type(a) not in (int, float):
         raise TypeError("a must be an integer")
-    if type(b) not in [int, float]:
+    if type(b) not in (int, float):
         raise TypeError("b must be an integer")
     return int(a) + int(b)
