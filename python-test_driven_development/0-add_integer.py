249980504f779707d5ecec4d563b02a837e04595
Gwendal-B <12309@holbertonstudents.com>
2026-01-21 09:30:02 +0100

    fix: Corrected filename from Readme.md to README.md

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -6,8 +6,8 @@ Raises TypeError if inputs are not integers or floats.
 def add_integer(a, b=98):
     """Adds two integers and returns the result as an integer.
         a (int, float): The first integer.
-        b (int, float): The second integer (default is 98)."""
-
+        b (int, float): The second integer (default is 98).
+    """
     if type(a) not in [int, float]:
         raise TypeError("a must be an integer")
     if type(b) not in [int, float]:
