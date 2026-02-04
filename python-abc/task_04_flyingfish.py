2674a177a1dbd87e7dec56dc6568f0d8af4bf145
Gwendal-B <12309@holbertonstudents.com>
2026-02-04 12:44:16 +0100

    update task_04_flyingfish.py

--- python-abc/task_04_flyingfish.py
+++ python-abc/task_04_flyingfish.py
@@ -5,7 +5,7 @@ FlyingFish."""
 
 class Fish:
     def swim(self):
-        print("Thes fish is swimming")
+        print("The fish is swimming")
 
     def habitat(self):
         print("The fish lives in water")
@@ -21,10 +21,10 @@ class Bird:
 
 class FlyingFish(Fish, Bird):
     def swim(self):
-        print("The flying fish is swimming !")
+        print("The flying fish is swimming!")
 
     def fly(self):
-        print(f"The flying fish is soaring!")
+        print("The flying fish is soaring!")
 
     def habitat(self):
         print("The flying fish lives both in water and the sky!")
