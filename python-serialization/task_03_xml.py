ec8d5c74555d14292c2ed98b69d5f40790d907d8
Gwendal-B <12309@holbertonstudents.com>
2026-02-10 12:28:19 +0100

    added files for python-serialization project

---
+++ python-serialization/task_03_xml.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+
+import xml.etree.ElementTree as ET
+
+
+def serialize_to_xml(dictionary, filename):
+    root = ET.Element("data")
+
+    for key, value in dictionary.items():
+        element = ET.SubElement(root, key)
+        element.text = str(value)
+
+    ET.indent(root, space="     ")
+
+    tree = ET.ElementTree(root)
+    tree.write(filename, encoding='utf-8', xml_declaration=False)
+
+def deserialize_from_xml(filename):
+    tree = ET.parse(filename)
+    root = tree.getroot()
+
+    result = {}
+
+    for element in root:
+        result[element.tag] = element.text
+
+    return result
