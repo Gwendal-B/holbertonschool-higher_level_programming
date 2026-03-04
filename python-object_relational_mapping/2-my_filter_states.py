4f3f4cec2618849fa224493a4fd27c484f4d30bd
Gwendal-B <12309@holbertonstudents.com>
2026-03-04 11:29:21 +0100

    fix: line 26 too long

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -23,7 +23,10 @@ if __name__ == "__main__":
 
     cursor = db.cursor()
 
-    query = "SELECT * FROM states WHERE name = '{}' ORDER BY id ASC".format(state_name)
+    query = (
+        "SELECT * FROM states WHERE name = '{}' ORDER BY id ASC"
+        .format(state_name)
+    )
     cursor.execute(query)
 
     rows = cursor.fetchall()
