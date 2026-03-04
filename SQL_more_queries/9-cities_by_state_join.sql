37383e53c62750adc29c9bcfcc1a56b7c26a1793
Gwendal-B <12309@holbertonstudents.com>
2026-03-04 11:18:51 +0100

    fix: suppr alias

--- SQL_more_queries/9-cities_by_state_join.sql
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -1,5 +1,5 @@
 -- Lists all cities with their corresponding state name using a JOIN
-SELECT cities.id, cities.name, states.name AS state
+SELECT cities.id, cities.name, states.name
 FROM cities
 JOIN states ON cities.state_id = states.id
 ORDER BY cities.id ASC;
