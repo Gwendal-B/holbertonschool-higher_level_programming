9ed2c1debe9da3ecbab80ad900302e9406af734c
Gwendal-B <12309@holbertonstudents.com>
2026-03-04 13:36:38 +0100

    Fix pycodestyle for task 8

--- python-object_relational_mapping/8-model_state_fetch_first.py
+++ python-object_relational_mapping/8-model_state_fetch_first.py
@@ -1,10 +1,5 @@
 #!/usr/bin/python3
-"""
-Prints the first State object from the database hbtn_0e_6_usa 
-using SQLAlchemy ORM.
-The first State is determined by the lowest id. If no State 
-exists, prints 'Nothing'.
-"""
+"""Prints the first State object from the database hbtn_0e_6_usa."""
 import sys
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
@@ -15,8 +10,9 @@ if __name__ == "__main__":
     user, password, db = sys.argv[1], sys.argv[2], sys.argv[3]
 
     # Connect to the MySQL database
-    engine = (create_engine('mysql+mysqldb://{}:{}@localhost/{}'
-                            .format(user, password, db), pool_pre_ping=True)
+    engine = create_engine(
+        'mysql+mysqldb://{}:{}@localhost/{}'.format(user, password, db),
+        pool_pre_ping=True
     )
 
     # Create a session
