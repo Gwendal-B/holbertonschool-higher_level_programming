04cde5d905b83c885f11d03b04139ce90c8775c2
Gwendal-B <12309@holbertonstudents.com>
2026-03-18 21:10:07 +0100

    correction checker

--- javascript-warm_up/9-add.js
+++ javascript-warm_up/9-add.js
@@ -4,7 +4,7 @@ function add(a, b) {
   return a + b;
 }
 
-const a = parseInt(process.argv[2]);
-const b = parseInt(process.argv[3]);
+const a = parseInt(process.argv[2]) || 0;
+const b = parseInt(process.argv[3]) || 0;
 
 console.log(add(a, b));
